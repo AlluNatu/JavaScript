552cb2b942082cdf940e2ab422c08ff50d54c02b
AlluNatu <aleksi.natunen@hotmail.fi>
2024-09-24 16:39:37 +0300

    Week4

---
+++ week4/js/week4.js
@@ -0,0 +1,53 @@
+const addDataButton = document.getElementById("submit-data")
+const addDataText = document.getElementById("input-show")
+const divContainer = document.getElementById("show-container")
+
+addDataButton.addEventListener("click", async function() {
+        let textFromData = ""
+        divContainer.innerHTML = ""
+        let fetchURL = "https://api.tvmaze.com/search/shows?q="
+        textFromData = addDataText.value
+        const response = await fetch(fetchURL+textFromData)
+        const JSONfile = await response.json()
+        let movieDetails = JSONfile
+        movieDetails.forEach(element => {
+            let movie = element.show
+            let div1 = document.createElement('div')
+            let div2 = document.createElement('div')
+            let img = document.createElement('img')
+            let h1 = document.createElement('h1')
+            let p = document.createElement('p')
+    
+            div1.classList.add("show-data")
+            div2.classList.add("show-info")
+
+
+            if (movie.image == null) {
+                console.log("No image")
+            }
+
+            else{
+                img.src = movie.image.medium
+            
+                div1.appendChild(img)
+            }
+    
+            h1.append(movie.name)
+            p.innerHTML = movie.summary
+            div2.appendChild(h1)
+            div2.appendChild(p)
+            div1.appendChild(div2)
+    
+    
+            divContainer.appendChild(div1)
+        });
+
+
+})
+addDataText.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault()
+        document.getElementById('submit-data').click()
+}
+})
+
